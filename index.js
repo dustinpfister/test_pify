d0449910a3b0b391291ad1c78471890fb0d1f870
dustinpfister <dustin.pfister@gmail.com>
2018-02-27 18:53:37 -0500

    more code examples

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 let pify = require('pify'),
 fs = require('fs');
 
-fs = pify(fs);
+pify(fs.readFile)('readme.md').then(function (text) {
 
-fs.readFile('index.js').then(function (text) {
-
+    // the contents of the readme
     console.log(text.toString());
+
+}).catch (function (e) {
+
+    // something went wrong
+    console.log(e);
+
+});
+
+fs.readFile('readme.md', function (e, text) {
+
+    // something went wrong
+    if (e) {
+
+        console.log(e);
+
+    } else {
+
+        // the contents of the readme
+        console.log(text.toString());
+
+    }
 
 });
